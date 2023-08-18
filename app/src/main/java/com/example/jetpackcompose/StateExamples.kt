31ee805bbb8c2898cc060107085e0dcec0d3359b
Viraj76 <virajg.7680@gmail.com>
2023-08-19 01:14:01 +0530

    learned about states and state hoisting

--- app/src/main/java/com/example/jetpackcompose/StateExamples.kt
+++ app/src/main/java/com/example/jetpackcompose/StateExamples.kt
@@ -1,4 +1,73 @@
 package com.example.jetpackcompose
 
-class StateExamples {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun NotificationScreen(){
+    val count = rememberSaveable{mutableStateOf(0)}  // it is
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(1f)
+    ) {
+        SendNotification(count.value) { count.value++ }
+        MessageBar(count.value)
+    }
+}
+
+@Composable
+fun SendNotification(count: Int, increment: () -> Unit) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = "You have sent ${count} notifications")
+        Button(
+            onClick = {increment()}
+        ) {
+            Text(text = "Send Notification")
+        }
+    }
+}
+
+@Composable
+fun MessageBar(count: Int) {
+    Card (
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        )
+    ){
+        Row(
+            Modifier.padding(6.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                imageVector = Icons.Outlined.Favorite,
+                contentDescription = "",
+                Modifier.padding(4.dp)
+            )
+            Text(text = "Message sent so far - $count")
+        }
+    }
+    
 }
